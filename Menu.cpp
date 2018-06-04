907254802978e44ad854c6967c3fcb2d9a4a2734
evrenke <evrenkeskin300@gmail.com>
2018-06-04 00:17:50 -0700

    Add files via upload

--- Menu.cpp
+++ Menu.cpp
@@ -1,38 +1,40 @@
-
 #include <string>
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
-	Menu basic = Menu();
-	basic.runMenu();
-
-	return 0;
-}
-
 class Menu
 {
 private:
-	bool isUsingMenu;
-	string introductions[100];
+	
 	void outputIntroduction()
 	{
-
+		for (int ind = 0; ind < introductions->length; ind++)
+		{
+			cout << introductions[ind] << endl;
+		}
 	}
-	void getInput()
-	{
-
-	}
+	void getInput(){}
 	void runOptions()
-	{
+	{	
 		isUsingMenu = false;
 	}
+protected:
+	bool isUsingMenu;
+	string* introductions;
 public:
 	Menu()
 	{
 		isUsingMenu = true;
+		introductions = new string[]
+		{
+			"This is the parent menu class",
+			"If you are seeing this, something is wrong"
+		};
+	}
+	~Menu()
+	{
+		delete introductions;
 	}
 
 	void runMenu()
@@ -44,5 +46,4 @@ public:
 			runOptions();
 		} while (isUsingMenu);
 	}
-	
 };
