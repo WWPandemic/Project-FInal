e6d222357244504875da2872b54c2b2cc439982a
canesul <39573980+canesul@users.noreply.github.com>
2018-06-11 16:48:47 -0700

    Update Inventory.h

--- Inventory.h
+++ Inventory.h
@@ -6,6 +6,8 @@
 class Inventory : public Menu
 {
 private:
+	Bundle* books;
+
 	//outputIntroduction unmodified
 	void getInput()
 	{
@@ -17,39 +19,62 @@ private:
 			chosenOption = std::stoi(input);
 		} while (chosenOption < 0 || chosenOption > 5);
 	}
-	void runOptions()
-	{
-		switch (chosenOption)
-		{
-		case 5: break;
-		default: break;
-		}
-	};
-	//Bundle books;
-	void LookUpBooks() {}
-	void AddNewBooks() {}
-	void DeleteBooks() {}
-	void EditBookInformation() {}
+
+	void runOptions();
+
+	void LookUpBooks();
+	void AddNewBooks();
+	void DeleteBooks();
+	void EditBookInformation();
+
+	int searchByTitle(std::string title);
+	int searchByISBN(int ISBN);
+
 public:
 	Inventory()
 	{
 		chosenOption = 5;//defaults to main menu
-		 
-		introductions[0] =	"---------------------------------------------------------------------------",
-		introductions[1] =	"                     Welcome to the Inventory Module:",
-		introductions[2] =	"This module allows you to look at the books inventory",
-		introductions[3] =	"And all change information related to the inventory",
-		introductions[4] =	"You can use this module to:",
-		introductions[5] =	"1 - Look Up Books",
-		introductions[6] =	"   Use a name or the ISBN of a book to find its information",
-		introductions[7] =	"2 - Add New Books",
-		introductions[8] =	"    Enter all information relating to a book and add it to the inventory",
-		introductions[9] =	"3 - Delete Books",
-		introductions[10] =	"    Find and remove any books from the inventory",
-		introductions[11] =	"4 - Change informations",
-		introductions[12] =	"    Find any book and edit its information",
-		introductions[13] =	"5 - Go back to main menu",
-		introductions[14] =	"---------------------------------------------------------------------------"
+
+		introductions[0] = "---------------------------------------------------------------------------",
+			introductions[1] = "                     Welcome to the Inventory Module:",
+			introductions[2] = "This module allows you to look at the books inventory",
+			introductions[3] = "And all change information related to the inventory",
+			introductions[4] = "You can use this module to:",
+			introductions[5] = "1 - Look Up Books",
+			introductions[6] = "   Use a name or the ISBN of a book to find its information",
+			introductions[7] = "2 - Add New Books",
+			introductions[8] = "    Enter all information relating to a book and add it to the inventory",
+			introductions[9] = "3 - Delete Books",
+			introductions[10] = "    Find and remove any books from the inventory",
+			introductions[11] = "4 - Change informations",
+			introductions[12] = "    Find any book and edit its information",
+			introductions[13] = "5 - Go back to main menu",
+			introductions[14] = "---------------------------------------------------------------------------";
+			usedIntroLines = 15;
+	};
+
+	Inventory(Bundle* b)
+	{
+		chosenOption = 5;//defaults to main menu
+		books = b;
+
+		introductions[0] = "---------------------------------------------------------------------------",
+			introductions[1] = "                     Welcome to the Inventory Module:",
+			introductions[2] = "This module allows you to look at the books inventory",
+			introductions[3] = "And all change information related to the inventory",
+			introductions[4] = "You can use this module to:",
+			introductions[5] = "1 - Look Up Books",
+			introductions[6] = "   Use a name or the ISBN of a book to find its information",
+			introductions[7] = "2 - Add New Books",
+			introductions[8] = "    Enter all information relating to a book and add it to the inventory",
+			introductions[9] = "3 - Delete Books",
+			introductions[10] = "    Find and remove any books from the inventory",
+			introductions[11] = "4 - Change informations",
+			introductions[12] = "    Find any book and edit its information",
+			introductions[13] = "5 - Go back to main menu",
+			introductions[14] = "---------------------------------------------------------------------------";
 		usedIntroLines = 15;
 	};
+
+	~Inventory() {}
 };
