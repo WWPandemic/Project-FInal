282552c5c4af3a327ba4c44583ba0f0abdc25036
evrenke <evrenkeskin300@gmail.com>
2018-06-11 17:32:28 -0700

    Update CIS22B_Final.cpp

--- CIS22B_Final.cpp
+++ CIS22B_Final.cpp
@@ -1,21 +1,19 @@
-#include <iostream>
-#include <string>
-#include "Bundle.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include "BookReader.h"
+#include "MainMenu.h"
 
 using namespace std;
 
-int main() {
-	Date testDate1(6, 5, 2018);
-	Date testDate2(7, 4, 2018);
-	Date testDate3(testDate1);
-	Book testBook1("title", "author", "publisher", 1, testDate1, 12, 5.5, 10.75);
-	Book testBook2 = testBook1;
-	Bundle testBundle(5);
-	testBundle.addBook(testBook1);
-	testBundle.addBook(testBook2);
-	cout << testBundle << endl;
-	//testBundle.printAll();
-	testBundle.addBook(testBook1);
+int main()
+{
+	BookReader reader;
+	Bundle b = reader.createBundle();
+	//implement reading bundle from samplebooks.txt
+	MainMenu mainmenu(b);
+	mainmenu.runMenu();
+
+	reader.exportBundle(b);
+
 	cout << "Please press  the <Enter> key to exit" << endl;
 	cin.get();
 	return 0;
