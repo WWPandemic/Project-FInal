cb8aca15978b8d27d50f85a1e30b2b075fd5af58
terchiem <39573970+terchiem@users.noreply.github.com>
2018-06-15 15:10:24 -0700

    Add files via upload

--- BookReader.cpp
+++ BookReader.cpp
@@ -70,20 +70,19 @@ std::string BookReader::stringISBN(int isbn)		// Convert isbn back to string and
 std::string BookReader::stringDate(Date date)		// Convert date object back to string
 {
 	std::string str = "";
-	if (*(date.getDate() + 0) < 10)
+	if (date[0] < 10)
 		str.append("0");
-	str.append(std::to_string(*(date.getDate() + 0)));
-	if (*(date.getDate() + 1) < 10)
+	str.append(std::to_string(date[0]));
+	if (date[1] < 10)
 		str.append("0");
-	str.append(std::to_string(*(date.getDate() + 1)));
-	str.append(std::to_string(*(date.getDate() + 2)));
+	str.append(std::to_string(date[1]));
+	str.append(std::to_string(date[2]));
 	return str;
 }
 
-Bundle BookReader::createBundle()				// Creates bundle from input file
+void BookReader::createBundle(Bundle b)				// Creates bundle from input file
 {
 	std::string bookInfo;			// All book info in a single string
-	Bundle bundle(30);				// Created bundle with expected max of 30
 
 	// Read each line, separate into variables and create a new book
 	while (std::getline(inFile, bookInfo))
@@ -92,51 +91,50 @@ Bundle BookReader::createBundle()				// Creates bundle from input file
 		const int PARAM = 8;					// Number of book parameters
 		std::string info[PARAM];				// Array to store book info in
 		splitInfo(info, bookInfo, PARAM);		// Call function to split bookInfo into array
+		int count = 0;							// Number of books
 
 		// Convert date info into Date object
 		Date date(stoi(info[4].substr(0, 2)), stoi(info[4].substr(2, 2)), stoi(info[4].substr(4, 4)));
 
-		// Add book to bundle
-		bundle.addBook(info[0], info[1], info[2], stoi(info[3]), date, stoi(info[5]), stod(info[6]), stod(info[7]));
+		// Add book to bundle, add one to count
+		b[count++] = Book(info[0], info[1], info[2], stoi(info[3]), date, stoi(info[5]), stod(info[6]), stod(info[7]));
 
 		// Check bundle size
-		bundle.ensureSize(bundle.getSize());
+		//bundle.ensureSize(bundle.getSize());
 	}
-
-	return Bundle(bundle);
 }
 
-// **temp**
-void BookReader::exportBundle(Bundle bundle)			// Sends bundle to output file
-{
-	// Open output file
-	std::cout << "Exporting book inventory data, please enter the file path location ...\n";
-	do {
-		std::string userOutFile;
-		std::cout << "Output File: ";
-		getline(std::cin, userOutFile);			// User enters output file location
-		outFile.open(userOutFile);
-
-		if (!outFile) {			// Validate output file
-			std::cout << "Could not find the specified file, please try again.\n\n";
-		}
-		else {
-			std::cout << "File successfully opened!\n\n";
-		}
-	} while (!outFile);
-	
-	// Write to output file
-	Book *write = bundle.getBundle();
-	for (int i = 0; i < bundle.getSize(); i++) {
-		outFile << (write + i)->getTitle() << ";" <<
-			(write + i)->getAuthor() << ";" <<
-			(write + i)->getPublisher() << ";" <<
-			stringISBN((write + i)->getISBN()) << ";" <<
-			stringDate((write + i)->getDateAdded()) << ";" <<
-			(write + i)->getQuantityOnHand() << ";" <<
-			(write + i)->getWholesaleCost() << ";" <<
-			(write + i)->getRetailPrice() << "\n";
-	}
-
-	outFile.close();
-}
+//// **temp**
+//void BookReader::exportBundle(Bundle bundle)			// Sends bundle to output file
+//{
+//	// Open output file
+//	std::cout << "Exporting book inventory data, please enter the file path location ...\n";
+//	do {
+//		std::string userOutFile;
+//		std::cout << "Output File: ";
+//		getline(std::cin, userOutFile);			// User enters output file location
+//		outFile.open(userOutFile);
+//
+//		if (!outFile) {			// Validate output file
+//			std::cout << "Could not find the specified file, please try again.\n\n";
+//		}
+//		else {
+//			std::cout << "File successfully opened!\n\n";
+//		}
+//	} while (!outFile);
+//	
+//	// Write to output file
+//	Book *write = bundle.getBundle();
+//	for (int i = 0; i < bundle.getSize(); i++) {
+//		outFile << (write + i)->getTitle() << ";" <<
+//			(write + i)->getAuthor() << ";" <<
+//			(write + i)->getPublisher() << ";" <<
+//			stringISBN((write + i)->getISBN()) << ";" <<
+//			stringDate((write + i)->getDateAdded()) << ";" <<
+//			(write + i)->getQuantityOnHand() << ";" <<
+//			(write + i)->getWholesaleCost() << ";" <<
+//			(write + i)->getRetailPrice() << "\n";
+//	}
+//
+//	outFile.close();
+//}
