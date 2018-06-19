ffd21fce110e6bbb8344e00415b248a10bc8b968
evrenke <evrenkeskin300@gmail.com>
2018-06-18 17:33:23 -0700

    Update CIS22B_Final.cpp

--- CIS22B_Final.cpp
+++ CIS22B_Final.cpp
@@ -1,42 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <string>
-#include "Bundle.h"
+#include "MainMenu.h"
+#include "BookReader.h"
 
-using namespace std;
+int main() 
+{
+	BookReader br;
+	Bundle books = br.createBundle();
 
-int main() {
-	Date testDate1(6, 5, 2018);
+	MainMenu mainmenu = MainMenu(books);
+	mainmenu.runMenu();
 
-	Book testBook1("a", "*", "*", 1, testDate1, 2, 1, 1);
-	Book testBook2("b", "*", "*", 1, testDate1, 2, 1, 1);
-	Book testBook3("c", "*", "*", 1, testDate1, 2, 1, 1);
-	Book testBook4("d", "*", "*", 1, testDate1, 2, 1, 1);
-	Book testBook5("e", "*", "*", 1, testDate1, 2, 1, 1);
-	
-	Bundle testBundle(25);
-	testBundle.addBook(testBook5);
-	testBundle.addBook(testBook3);
-	testBundle.addBook(testBook2);
-	testBundle.addBook(testBook4);
-	testBundle.addBook(testBook1);
+	br.exportBundle(books);
 
-	testBundle.addBook("f", "*", "*", 1, testDate1, 2, 1, 1);
-	testBundle.addBook("e", "*", "*", 1, testDate1, 2, 1, 1);
-	testBundle.addBook("z", "*", "*", 1, testDate1, 2, 1, 1);
-	testBundle.addBook("h", "1*", "*", 1, testDate1, 2, 1, 1);
-	testBundle.addBook("e", "1*", "*", 1, testDate1, 2, 1, 1);
-	
-	cout << testBundle << endl;
-	int index = testBundle.findFirstIndex("e");
-	cout << index << endl;
-
-	int index2[2] = { 0,0 };
-	testBundle.findIndexBounds("e",index2);
-	cout << "is something happening" << endl;
-	cout << index2[0] << " " << index2[1] << endl;
-
-	cout << "Please press the <Enter> key to exit" << endl;
-	cin.get();
+	std::cout << "Please press the <Enter> key to exit" << std::endl;
+	std::cin.get();
 	return 0;
 }
