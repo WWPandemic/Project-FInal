7124d672cc9292b7036e11be0b4d2ef89d896c17
terchiem <39573970+terchiem@users.noreply.github.com>
2018-06-19 13:40:28 -0700

    Commented

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -3,21 +3,17 @@
 #include <iomanip>
 #include "ShoppingCart.h"
 
-//**************************
-//* Constructor definition *
-//**************************
-
-ShoppingCart::ShoppingCart()
-{
-	subtotal = 0.0;
-	totalCost = 0.0;
-	totalProfit = 0.0;
-	amountPaid = 0.0;
-	change = 0.0;
-	completeCart = false;
-}
-
-ShoppingCart::ShoppingCart(Bundle &b)
+/*
+Constructor:	ShoppingCart
+Author:			Terry Chiem
+Parameters:		Bundle *b - A pointer to a reference of the main bundle
+Returns:		ShoppingCart object
+Description:	This constructor creates a ShoppingCart which then
+				allows the user to add books to it. After the addition
+				process is finished, all of the values are calculated
+				and assigned to the variables.
+*/
+ShoppingCart::ShoppingCart(Bundle *b)
 {
 	subtotal = 0.0;
 	totalCost = 0.0;
@@ -31,78 +27,138 @@ ShoppingCart::ShoppingCart(Bundle &b)
 	calculateChange();
 	validateCart();
 }
-
-//************************
-//* Accessor definitions *
-//************************
-
+/*
+Function:		getSubtotal
+Author :		Terry Chiem
+Parameters :	none
+Returns :		double
+Description :	An accessor for subtotal
+*/
 double ShoppingCart::getSubtotal() const		
 {
 	return subtotal;
 }
-
+/*
+Function:		getTotalCost
+Author :		Terry Chiem
+Parameters :	none
+Returns :		double
+Description :	An accessor for totalCost
+*/
 double ShoppingCart::getTotalCost() const		
 {
 	return totalCost;
 }
-
+/*
+Function:		getTotalProfit
+Author :		Terry Chiem
+Parameters :	none
+Returns :		double
+Description :	An accessor for totalProfit
+*/
 double ShoppingCart::getTotalProfit() const	
 {
 	return totalProfit;
 }
-
+/*
+Function:		getAmountPaid
+Author :		Terry Chiem
+Parameters :	none
+Returns :		double
+Description :	An accessor for amountPaid
+*/
 double ShoppingCart::getAmountPaid() const	
 {
 	return amountPaid;
 }
-
+/*
+Function:		getChange
+Author :		Terry Chiem
+Parameters :	none
+Returns :		double
+Description :	An accessor for change
+*/
 double ShoppingCart::getChange() const	
 {
 	return change;
 }
-
+/*
+Function:		getComplete
+Author :		Terry Chiem
+Parameters :	none
+Returns :		bool
+Description :	An accessor for completeCart
+*/
 bool ShoppingCart::getComplete() const
 {
 	return completeCart;
 }
-
-// Calculate and return tax amount
+/*
+Function:		getTax
+Author :		Terry Chiem
+Parameters :	none
+Returns :		double
+Description :	Calculates the tax by multiplying the
+				subtotal with the tax rate.
+*/
 double ShoppingCart::getTax() const		
 {
 	return subtotal * (TAX_RATE/100);
 }
-
-
-
-//***********************
-//* Mutator definitions *
-//***********************
-
-// Add amount to subtotal
+/*
+Function:		addSubtotal
+Author :		Terry Chiem
+Parameters :	double
+Returns :		none
+Description :	Adds the amount to subtotal.
+*/
 void ShoppingCart::addSubtotal(double c)			
 {
 	subtotal += c;
 }
-
-// Add amount to totalProfit
+/*
+Function:		addTotalProfit
+Author :		Terry Chiem
+Parameters :	double
+Returns :		none
+Description :	Adds the amount to totalProfit.
+*/
 void ShoppingCart::addTotalProfit(double p)			
 {
 	totalProfit += p;
 }
-
-// Calculate totalCost
+/*
+Function:		calculateTotal
+Author :		Terry Chiem
+Parameters :	none
+Returns :		none
+Description :	Calculates totalCost by adding the subtotal
+				with the tax owed.
+*/
 void ShoppingCart::calculateTotal()					
 {
 	totalCost = subtotal + getTax();
 }
-
-// Calculate change
+/*
+Function:		calculateChange
+Author :		Terry Chiem
+Parameters :	none
+Returns :		none
+Description :	Calculates change by subtracting the total
+				cost from the amount paid.
+*/
 void ShoppingCart::calculateChange()				
 {
 	change = amountPaid - totalCost;
 }
-
-// Input value for amountPaid
+/*
+Function:		processPay
+Author :		Terry Chiem
+Parameters :	none
+Returns :		none
+Description :	Prompts the user to enter the amount the customer paid. 
+				Amount is assigned to amountPaid.
+*/
 void ShoppingCart::processPay()						
 {
 	// If there are no items in cart
@@ -148,28 +204,34 @@ void ShoppingCart::processPay()
 		amountPaid = paid;
 	}
 }
-
-//************************
-//* Function definitions *
-//************************
-
-// Get confirmation
+/*
+Function:		getConfirmation
+Author :		Terry Chiem
+Parameters :	none
+Returns :		string
+Description :	Gets the next input and returns it as a string.
+*/
 std::string ShoppingCart::getConfirmation() {
 	std::string input;
 	getline(std::cin, input);
 	return input;
 }
-
-// Add items to cart
-void ShoppingCart::addItems(Bundle &b) {			
+/*
+Function:		addItems
+Author :		Terry Chiem
+Parameters :	Bundle *b - A pointer to a reference of the main bundle
+Returns :		none
+Description :	Prompts the user for book ISBNs to add to the current cart.
+*/
+void ShoppingCart::addItems(Bundle *b) {
 	int count = 0;			// Number of books in cart
 	std::string repeat;		// To loop adding of books
 
 	// Sort bundle by ISBN for searchability
-	sortISBN(b);
+	Bundle sorted = sortISBN(b);
 
 	std::cout << "Please enter Book ISBNs to add items to the cart.\n";
-	do {		
+	do {
 		// Prompt user for ISBN
 		int userISBN;
 		std::cout << "ISBN: ";
@@ -185,28 +247,28 @@ void ShoppingCart::addItems(Bundle &b) {
 		}
 		std::cout << std::endl;
 
-		// Search main bundle for userISBN
-		int pos = searchISBN(b, userISBN);
+		// Search sorted bundle for userISBN
+		int pos = searchISBN(sorted, userISBN);
 
 		// If book is found
 		if (pos >= 0) {
 			// Print book details
 			std::cout << "\n---------------------------------------------------------------------------\n\n";
-			printBook(b[pos]);
-			std::cout << "Stock: " << b[pos].getQuantity() << std::endl;
+			printBook(sorted[pos]);
+			std::cout << "Stock: " << sorted[pos].getQuantity() << std::endl;
 			std::cout << "---------------------------------------------------------------------------\n";
 
 			// Prompt user for confirmation to add to cart
 			std::cout << "\nAdd this item to cart? (y/n): ";
 			std::string confirm = getConfirmation();
-			
+
 			// If add is confirmed
 			if (confirm == "y" || confirm == "Y") {
 				// If item is in stock
-				if (b[pos].getQuantity() > 0) {
+				if (sorted[pos].getQuantity() > 0) {
 					// Add book to cart
-					addBook(b[pos]);
-					std::cout << "'" << b[pos].getTitle() << "' added to cart.\n\n";
+					addBook(sorted[pos]);
+					std::cout << "'" << sorted[pos].getTitle() << "' added to cart.\n\n";
 
 					// Check cart size
 					ensureSize(++count);
@@ -232,36 +294,52 @@ void ShoppingCart::addItems(Bundle &b) {
 		std::cout << std::endl;
 	} while (repeat == "y" || repeat == "Y");
 }
-
-// Sort main bundle by isbn
-void ShoppingCart::sortISBN(Bundle &b)				
+/*
+Function:		sortISBN
+Author :		Terry Chiem
+Parameters :	Bundle *b - A pointer to a reference of the main bundle
+Returns :		Bundle
+Description :	Performs a selective sort by ISBN on the main bundle and
+				returns a copy of it.
+*/
+Bundle ShoppingCart::sortISBN(Bundle *b)
 {
-	for (int ind = 0; ind < b.getSize(); ind++)
+	Bundle sorted = *b;
+	// Perform a selective sort by ISBN
+	for (int ind = 0; ind < b->getSize(); ind++)
 	{
-		int minISBN = b[ind].getISBN();
+		int minISBN = sorted[ind].getISBN();
 		int swapInd = ind;
-		for (int target = ind; target < b.getSize(); target++)
+		for (int target = ind; target < b->getSize(); target++)
 		{
-			if (b[target].getISBN() < minISBN)
+			if (sorted[target].getISBN() < minISBN)
 			{
 				swapInd = target;
-				minISBN = b[target].getISBN();
+				minISBN = sorted[target].getISBN();
 			}
 		}
 		if (swapInd != ind)
 		{
-			m_books.swap(b[ind], b[swapInd]);
+			sorted.getArray().swap(sorted[ind], sorted[swapInd]);
 		}
 	}
+	return sorted;
 }
-
-// Search main bundle for isbn, returns index position or -1
-int ShoppingCart::searchISBN(Bundle &b, int search)		
+/*
+Function:		searchISBN
+Author :		Terry Chiem
+Parameters :	Bundle *b - A pointer to a reference of the main bundle
+				int search - The search item (ISBN) specified by the user
+Returns :		int
+Description :	Performs a binary search for the search item and returns
+				its index position if found, a -1 if not found.
+*/
+int ShoppingCart::searchISBN(Bundle b, int search)
 {
 	int first = 0;
 	int last = b.getSize();
 	int mid = 0;
-
+	// Perform a binary search for item
 	while (first <= last) {
 		mid = (first + last) / 2;
 		if (b[mid].getISBN() == search) {
@@ -280,8 +358,14 @@ int ShoppingCart::searchISBN(Bundle &b, int search)
 	// If not found, return -1
 	return -1;
 }
-
-// Calculate all total values
+/*
+Function:		processTotals
+Author :		Terry Chiem
+Parameters :	none
+Returns :		none
+Description :	Goes through every book in the cart and adds its
+				values to the member variables.
+*/
 void ShoppingCart::processTotals()		
 {
 	for (int i = 0; i < getSize(); i++) {
@@ -290,8 +374,15 @@ void ShoppingCart::processTotals()
 	}
 	calculateTotal();
 }
-
-// Validate status of cart
+/*
+Function:		validateCart
+Author :		Terry Chiem
+Parameters :	none
+Returns :		none
+Description :	Determines if the cart is valid. If the cart is not
+				empty and paid off, sets the status to true and false
+				otherwise.
+*/
 void ShoppingCart::validateCart()		
 {
 	if (getSize() > 0 && amountPaid >= totalCost) {
@@ -301,16 +392,26 @@ void ShoppingCart::validateCart()
 		completeCart = false;
 	}
 }
-
-// Print book details
+/*
+Function:		printBook
+Author :		Terry Chiem
+Parameters :	Book book - Item to be printed
+Returns :		none
+Description :	Prints out the book's details (title, author, price).
+*/
 void ShoppingCart::printBook(Book book) {
 	std::cout << std::setprecision(2) << std::fixed;
 	std::cout << book.getTitle() << std::endl;
 	std::cout << "by " << std::setw(57) << std::left << book.getAuthor() << "$" << book.getPrice() << std::endl;
 	std::cout << std::endl;
 }
-
-// Print all books in cart
+/*
+Function:		printAll
+Author :		Terry Chiem
+Parameters :	none
+Returns :		none
+Description :	Prints out every book in cart.
+*/
 void ShoppingCart::printAll() {
 	for (int i = 0; i < getSize(); i++)
 	{
--- ShoppingCart.h
+++ ShoppingCart.h
@@ -4,6 +4,7 @@
 class ShoppingCart : public Bundle
 {
 private:
+	// Cart variables
 	const double TAX_RATE = 9.0;	// Current city tax rate
 	double subtotal;				// Total cost before taxes
 	double totalCost;				// Total cost of ShoppingCart
@@ -11,11 +12,16 @@ private:
 	double amountPaid;				// Total paid by customer
 	double change;					// Change owed to customer
 	bool completeCart;				// If cart is valid or not
+	
+	void addSubtotal(double);		// Add amount to subtotal
+	void addTotalProfit(double);	// Add amount to totalProfit
+	void calculateTotal();			// Calculate totalCost
+	void calculateChange();			// Calculate change
+	void processPay();				// Input value for amountPaid
 
 public:
 	// Constructors
-	ShoppingCart();
-	ShoppingCart(Bundle &b);
+	ShoppingCart(Bundle *b);
 
 	// Accessors
 	double getSubtotal() const;				// Return subtotal
@@ -25,19 +31,12 @@ public:
 	double getChange() const;				// Return change
 	bool getComplete() const;				// Return complete cart status
 	double getTax() const;					// Return tax amount
-	
-	// Mutators
-	void addSubtotal(double);			// Add amount to subtotal
-	void addTotalProfit(double);		// Add amount to totalProfit
-	void calculateTotal();				// Calculate totalCost
-	void calculateChange();				// Calculate change
-	void processPay();					// Input value for amountPaid
 
 	// Functions
 	std::string getConfirmation();			// Get confirmation
-	void addItems(Bundle &b);				// Add items to cart
-	void sortISBN(Bundle &b);				// Sort bundle by isbn
-	int searchISBN(Bundle &b, int);			// Search main bundle for isbn
+	void addItems(Bundle *b);				// Add items to cart
+	Bundle sortISBN(Bundle *b);				// Sort bundle by isbn
+	int searchISBN(Bundle b, int);			// Search bundle for isbn
 	void processTotals();					// Calculate all total values
 	void validateCart();					// Validate status of cart
 	void printBook(Book);					// Print book details
