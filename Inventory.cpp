b9ac597fdaece576d704f24b12768d6c47865971
evrenke <evrenkeskin300@gmail.com>
2018-06-20 02:44:02 -0700

    Update Inventory.cpp

--- Inventory.cpp
+++ Inventory.cpp
@@ -2,6 +2,27 @@
 #include <cctype>
 #include "Inventory.h"
 
+void Inventory::getInput()
+{
+	do
+	{
+		std::cout << "What would you like to do?" << std::endl;
+		std::string input;
+		getline(std::cin, input);
+		try {
+			chosenOption = std::stoi(input);
+		}
+		catch (...)
+		{
+			chosenOption = -1;
+		}
+		if (chosenOption < 1 || chosenOption > 5)
+		{
+			std::cout << "Enter a valid number" << std::endl;
+		}
+	} while (chosenOption < 1 || chosenOption > 5);
+}
+
 void Inventory::runOptions()//Runs function selected earlier by user.
 {
 	switch (chosenOption)
@@ -29,26 +50,37 @@ void Inventory::runOptions()//Runs function selected earlier by user.
 Prompts the user to enter the title or ISBN of desired book.
 If the user input begins and ends with a number it is assumed that the input is an ISBN and the search by ISBN function is called.
 If not the user input is assumed to be a title and the search by title function is called.
-If a valid index (i.e. greater than zero) is returned by either function the details of the book at that index are printed. 
+If a valid index (i.e. greater than zero) is returned by either function the details of the book at that index are printed.
 */
 void Inventory::LookUpBooks()
 {
 	std::cout << "Enter the title or ISBN of the book: ";
 	std::string info;
-	getline(std::cin, info);
-
-	int index;
-
-	if (std::isdigit(info.front()) && std::isdigit(info.back()))
-		index = searchByISBN(std::stoi(info));
-	else
-		index = searchByTitle(info);
+	
+	info = enterString();
+	int ISBNasked = 0;
+	try {
+		ISBNasked = std::stoi(info);
+	}
+	catch (...)
+	{
+		ISBNasked = -1;
+		//info is a name 
+	}
+	int index = searchByTitle(info);
+	
+	if (index == -1 && ISBNasked != -1)
+	{
+		index = searchByISBN(ISBNasked);
+	}
 
 	if (index < 0)
 		std::cout << "Book was not found.\n\n";
 	else
 	{
-		std::cout << (*books)[index] << std::endl;
+		std::cout << std::endl;
+		(*books)[index].print();
+		std::cout << std::endl;
 	}
 }
 
@@ -61,50 +93,62 @@ void Inventory::AddNewBooks()
 {
 	int count = 0;
 	std::cout << "How many books do you want to add?" << std::endl;
-	std::cin >> count;
+	count = enterInteger();
 
 	for (int i = 0; i < count; i++)
 	{
 		std::cout << "Enter the title for book #" << i + 1 << ": ";
 		std::string title;
 		std::cin.ignore();
-		getline(std::cin, title);
+		title = enterString();
 
 		std::cout << "Enter the author's name for book #" << i + 1 << ": ";
 		std::string author;
-		getline(std::cin, author);
+		author = enterString();
 
 		std::cout << "Enter the publisher for book #" << i + 1 << ": ";
 		std::string publisher;
-		getline(std::cin, publisher);
+		publisher = enterString();
 
 		std::cout << "Enter the ISBN for book #" << i + 1 << ": ";
 		int ISBN;
-		std::cin >> ISBN;
+		ISBN = enterInteger();
 
-		std::cout << "Enter today's date (MM/DD/YYYY): ";
-		std::string today;
-		std::cin >> today;
+		std::string date;
+		int month;
+		int day;
+		int year;
+		do {
+			std::cout << "Enter today's date (MMDDYYYY): ";
+			std::getline(std::cin, date);
+			try {
+				month = stoi(date.substr(0, 2));
+				day = stoi(date.substr(2, 2));
+				year = stoi(date.substr(4, 4));
+			}
+			catch (...) {
+				year = 0;
+			}
+			if (year == 0 || year < 2000) {
+				std::cout << "Invalid date.\n\n";
+			}
+		} while (year == 0 || year < 2000);
+		Date todayDate(month, day, year);
 
 		std::cout << "Enter the number on hand: ";
 		int quantity;
-		std::cin >> quantity;
+		quantity = enterInteger();
 
 		std::cout << "Enter the wholesale cost for book #" << i + 1 << ": ";
 		double cost;
-		std::cin >> cost;
+		cost = enterDouble();
 
 		std::cout << "Enter the retail price for book #" << i + 1 << ": ";
 		double price;
-		std::cin >> price;
-
-		int month = std::stoi(today.substr(0, 2));//Series of lines to find the date in a string entered MM/DD/YYYY
-		int day = std::stoi(today.substr(3, 2));  //And turn the string into three numbers representing the date.
-		int year = std::stoi(today.substr(6, 4));
-		Date todayDate(month, day, year);
-
-		(*books).addBook(title, author, publisher, ISBN, todayDate, quantity, cost, price);
+		price = enterDouble();
 		
+		(*books).addBook(title, author, publisher, ISBN, todayDate, quantity, cost, price);
+
 		std::cin.ignore();
 		std::cout << "Book added.\n\n";
 	}
@@ -124,21 +168,30 @@ void Inventory::DeleteBooks()
 {
 	int count = 0;
 	std::cout << "How many books do you want to delete?" << std::endl;
-	std::cin >> count;
+	count = enterInteger();
 
 	for (int i = 0; i < count; i++)
 	{
 		std::cout << "Enter the title or ISBN of the book: ";
 		std::string info;
 		std::cin.ignore();
-		getline(std::cin, info);
+		info = enterString();
 
-		int index;
+		int ISBNasked = 0;
+		try {
+			ISBNasked = std::stoi(info);
+		}
+		catch (...)
+		{
+			ISBNasked = -1;
+			//info is a name 
+		}
+		int index = searchByTitle(info);
 
-		if (std::isdigit(info.front()) && std::isdigit(info.back()))
-			index = searchByISBN(std::stoi(info));
-		else
-			index = searchByTitle(info);
+		if (index == -1 && ISBNasked != -1)
+		{
+			index = searchByISBN(ISBNasked);
+		}
 
 		if (index < 0)
 			std::cout << "Book was not found.\n\n";
@@ -162,14 +215,23 @@ void Inventory::EditBookInformation()
 {
 	std::cout << "Enter the title or ISBN of the book: ";
 	std::string info;
-	getline(std::cin, info);
+	info = enterString();
 
-	int index;
+	int ISBNasked = 0;
+	try {
+		ISBNasked = std::stoi(info);
+	}
+	catch (...)
+	{
+		ISBNasked = -1;
+		//info is a name 
+	}
+	int index = searchByTitle(info);
 
-	if (std::isdigit(info.front()) && std::isdigit(info.back()))
-		index = searchByISBN(std::stoi(info));
-	else
-		index = searchByTitle(info);
+	if (index == -1 && ISBNasked != -1)
+	{
+		index = searchByISBN(ISBNasked);
+	}
 
 	if (index < 0)
 		std::cout << "Book was not found.\n\n";
@@ -188,63 +250,78 @@ void Inventory::EditBookInformation()
 		std::cout << "---------------------------------------------------------------------------" << std::endl;
 		std::cout << "What will you edit?" << std::endl;
 		std::string input;
-		getline(std::cin, input);
-		switch (std::stoi(input))
+		int option = enterInteger();
+		switch (option)
 		{
 		case 1:
 			std::cout << "Enter a new title: ";
-			getline(std::cin, input);
+			input = enterString();
 			(*books)[index].setTitle(input);
 			std::cout << "Title set\n";
 			break;
 		case 2:
 			std::cout << "Enter a new author: ";
-			getline(std::cin, input);
+			input = enterString();
 			(*books)[index].setAuthor(input);
 			std::cout << "Author set\n";
 			break;
 		case 3:
 			std::cout << "Enter a new publisher: ";
-			getline(std::cin, input);
+			input = enterString();
 			(*books)[index].setPublisher(input);
 			std::cout << "Publisher set\n";
 			break;
 		case 4:
 			std::cout << "Enter a new ISBN: ";
 			int ISBN;
-			std::cin >> ISBN;
+			ISBN = enterInteger();
 			(*books)[index].setISBN(ISBN);
 			std::cout << "ISBN set\n";
 			break;
 		case 5:
 			int day[3];
 			std::cout << "Enter month added: ";
-			std::cin >> day[0];
+			do
+			{
+				day[0] = enterInteger();
+				if (day[0] < 0 || day[0] > 12)
+					std::cout << "Invalid Month\n";
+			} while (day[0] < 0 || day[0] > 12);
 			std::cout << "Enter day added: ";
-			std::cin >> day[1];
+			do
+			{
+				day[1] = enterInteger();
+				if (day[1] < 0 || day[1] > 31)
+					std::cout << "Invalid Date\n";
+			} while (day[1] < 0 || day[1] > 31);
 			std::cout << "Enter year added: ";
-			std::cin >> day[2];
+			do
+			{
+				day[2] = enterInteger();
+				if (day[2] < 1900 || day[2] > 2100)
+					std::cout << "Invalid Year\n";
+			} while (day[2] < 1900 || day[2] > 2100);
 			(*books)[index].setDate(Date(day));
 			std::cout << "Date set\n";
 			break;
 		case 6:
 			std::cout << "Enter quantity on hand: ";
 			int num;
-			std::cin >> num;
+			num = enterInteger();
 			(*books)[index].setQuantity(num);
 			std::cout << "Quantity on hand set\n";
 			break;
 		case 7:
 			std::cout << "Enter a new wholesale cost: ";
 			double cost;
-			std::cin >> cost;
+			cost = enterDouble();
 			(*books)[index].setCost(cost);
 			std::cout << "Cost set\n";
 			break;
 		case 8:
 			std::cout << "Enter a new retail price: ";
 			double price;
-			std::cin >> price;
+			price = enterDouble();
 			(*books)[index].setPrice(price);
 			std::cout << "Retail price set\n";
 			break;
@@ -276,12 +353,12 @@ int Inventory::searchByTitle(std::string title)
 			(*books)[min] = temp;
 		}
 	}
-	
+
 	int index = -1;
 	int first = 0;
 	int last = (*books).getSize();
 
-	while (first <= last && index == -1)
+	while (first < last && index == -1)
 	{
 		int mid = (first + last) / 2;
 		if ((*books)[mid].getTitle() == title)
@@ -291,7 +368,6 @@ int Inventory::searchByTitle(std::string title)
 		else
 			last = mid - 1;
 	}
-	
 	return index;
 }
 
@@ -348,5 +424,53 @@ int Inventory::searchByISBN(int ISBN)
 				index = i;
 		}
 	}
+
 	return index;
+}
+
+
+std::string Inventory::enterString()
+{
+	std::string input;
+
+	getline(std::cin, input);
+	return input;
+}
+
+int Inventory::enterInteger()
+{
+	std::string input;
+	int numberized = NULL;
+	do
+	{
+		getline(std::cin, input);
+		try
+		{
+			numberized = std::stoi(input);
+		}
+		catch (...)
+		{
+			
+		}
+	} while (numberized == NULL || numberized < 0);
+	return numberized;
+}
+
+double Inventory::enterDouble()
+{
+	std::string input;
+	double numberized = NULL;
+	do
+	{
+		getline(std::cin, input);
+		try
+		{
+			numberized = std::stod(input,NULL);
+		}
+		catch (...)
+		{
+
+		}
+	} while (numberized == NULL || numberized < 0);
+	return numberized;
 }
